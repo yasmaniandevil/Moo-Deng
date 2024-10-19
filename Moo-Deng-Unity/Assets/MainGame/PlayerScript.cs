8b17cea0c59f5f51a0b19eb3a6937bd6aebd5147
yasmaniandevil <yasmine.batniji@gmail.com>
2024-10-18 22:27:55 -0400

    started platform

--- Moo-Deng-Unity/Assets/MainGame/PlayerScript.cs
+++ Moo-Deng-Unity/Assets/MainGame/PlayerScript.cs
@@ -6,13 +6,19 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     public float speed = 5f;
+    public int jumpForce = 4;
+    public int gravityForce = 5;
+    public bool isjumpToggleEnabled = true;
+    private Transform playerTransform;
 
+    private bool isGrounded = true;
   
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        playerTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -22,5 +28,23 @@ public class PlayerScript : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         rb2d.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
+
+        if (Input.GetKey(KeyCode.Space) && isjumpToggleEnabled)
+        {
+            //apply upward force to the rigidbody for the jump
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+            Debug.Log("Jump");
+
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+           
+        }
+        //apply upward force to the rigidbody for the jump
+        rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
     }
 }
