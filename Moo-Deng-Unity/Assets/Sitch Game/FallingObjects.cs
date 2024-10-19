c56ba151a960c75d96f9279231afb6a3cd4b4d62
yasmaniandevil <yasmine.batniji@gmail.com>
2024-10-18 21:58:14 -0400

    got stacking semi working

--- Moo-Deng-Unity/Assets/Sitch Game/FallingObjects.cs
+++ Moo-Deng-Unity/Assets/Sitch Game/FallingObjects.cs
@@ -5,24 +5,19 @@ using DG.Tweening;
 
 public class FallingObjects : MonoBehaviour
 {
-    private Transform transformy;
-
     private bool isMoving = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        transformy = GetComponent<Transform>();
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
         MoveFunction();
-       
-        
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -30,7 +25,14 @@ public class FallingObjects : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             isMoving = false;
-            Debug.Log("turned false");
+            GetComponent<Rigidbody2D>().isKinematic = true;
+            //store currentPos relative to plate
+            //Vector3 localPos = transform.position - collision.transform.position;
+            //parent it to get its transform
+            transform.SetParent(collision.transform);
+            //maintain objpos relative to plate
+            //transform.localPosition = localPos;
+            Debug.Log("follow plate");
         }
     }
 
@@ -39,7 +41,7 @@ public class FallingObjects : MonoBehaviour
         if(isMoving == true)
         {
             float randomSpeed = Random.Range(.5f, 2f);
-            transformy.position = transform.position + new Vector3(0, -5 * randomSpeed * Time.deltaTime, 0);
+            transform.position = transform.position + new Vector3(0, -5 * randomSpeed * Time.deltaTime, 0);
 
 
             if (this.gameObject.transform.position.y <= -5)
